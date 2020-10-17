086ff1e51d03f13700dc5f8b6e1d93d43ef8e198
EC2 Default User <ec2-user@ip-172-31-92-17.ec2.internal>
2020-10-17 02:14:12 +0000

    server settings

--- server.js
+++ server.js
@@ -84,7 +84,6 @@ app.post("/", async (req, res) => {
 	}
 });
 
-
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on Port 3000");
+	console.log("Listening on Port 3000");
 });
