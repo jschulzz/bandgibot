e405eb947fb0be4fc49a4d8682b60ae8b963caec
John Schulz <jr.john.schulz@gmail.com>
2021-08-18 20:44:55 -0500

    birthdays were borked

--- cronjobs/birthdayCheck.js
+++ cronjobs/birthdayCheck.js
@@ -1,38 +1,39 @@
-import fs from 'fs'
-import axios from 'axios'
+import fs from "fs";
+import axios from "axios";
 import { sendMessage } from "../utils.js";
 
-
-export const birthdayCheck = async () => {
-	const dateString = `${new Date().getMonth() + 1}/${new Date().getDate()}`;
-	const birthdays = JSON.parse(fs.readFileSync(`birthdays.json`));
-	const birthdaysToday = birthdays[dateString] || [];
-	for (const bday of birthdaysToday) {
-		const { user_id, nickname, muted } = await memberDB.findOne({
-			name: bday,
-		});
-		const googleImageSearchRequest = await axios.get(
-			`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=Happy Birthday&rating=g`
-		);
-		const imageSet = googleImageSearchRequest.data.data;
-		const image = imageSet[Math.floor(Math.random() * imageSet.length)];
-		const message = `Happy Birthday ${nickname}!!`;
-		let attachments = [
-			{
-				type: "image",
-				url: `https://media0.giphy.com/media/${image.id}/giphy.gif`,
-			},
-		];
-		if (!muted) {
-			const loci = [[15, nickname.length + 1]];
-			const user_ids = [user_id];
-			attachments.push({
-				type: "mentions",
-				user_ids,
-				loci,
+export const birthdayCheck = (memberDB) => {
+	return async () => {
+		const dateString = `${new Date().getMonth() + 1}/${new Date().getDate()}`;
+		const birthdays = JSON.parse(fs.readFileSync(`birthdays.json`));
+		const birthdaysToday = birthdays[dateString] || [];
+		for (const bday of birthdaysToday) {
+			const { user_id, nickname, muted } = await memberDB.findOne({
+				name: bday,
 			});
+			const googleImageSearchRequest = await axios.get(
+				`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=Happy Birthday&rating=g`
+			);
+			const imageSet = googleImageSearchRequest.data.data;
+			const image = imageSet[Math.floor(Math.random() * imageSet.length)];
+			const message = `Happy Birthday ${nickname}!!`;
+			let attachments = [
+				{
+					type: "image",
+					url: `https://media0.giphy.com/media/${image.id}/giphy.gif`,
+				},
+			];
+			if (!muted) {
+				const loci = [[15, nickname.length + 1]];
+				const user_ids = [user_id];
+				attachments.push({
+					type: "mentions",
+					user_ids,
+					loci,
+				});
+			}
+			await sendMessage({ message, attachments });
+			console.log(`Happy Birthday to ${nickname}`);
 		}
-		await sendMessage({ message, attachments });
-		console.log(`Happy Birthday to ${nickname}`);
-	}
+	};
 };
